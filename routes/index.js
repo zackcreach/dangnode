5b5978d4afeea2f538a1e8c39057735b28b71049
zackcreach <zackcreach@gmail.com>
2017-11-04 10:24:19 -0400

    Added initial routing and templates.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('./../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  const zack = {name: 'Zack', age: 100, cool: true}
-  res.render('hello')
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-})
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
