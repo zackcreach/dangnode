c4c5f09b4ef2d75602f7ea6c21c640c949b13d49
zackcreach <zackcreach@gmail.com>
2017-11-17 18:48:21 -0500

    Added in forgot password functionality.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -19,5 +22,20 @@ exports.isLoggedIn = (req, res, next) => {
     return;
   }
   req.flash('error', 'Oops you must be logged in to do that!')
+  res.redirect('/login');
+}
+
+exports.forgot = async (req, res) => {
+  const user = await User.findOne( { email: req.body.email } );
+  if (!user) {
+    req.flash('error', 'A password reset has been mailed to you.');
+    return res.redirect('/login');
+  }
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+  await user.save();
+
+  const resetURL = `http://${req.headers.host}.account/reset/${user.resetPasswordToken}`;
+  req.flash('success', `You have been emailed a password reset link. ${resetURL}`);
   res.redirect('/login');
 }
--- routes/index.js
+++ routes/index.js
@@ -40,5 +40,6 @@ router.get('/logout', authController.logout);
 
 router.get('/account', authController.isLoggedIn, userController.account);
 router.post('/account', catchErrors(userController.updateAccount));
+router.post('/account/forgot', catchErrors(authController.forgot));
 
 module.exports = router;
